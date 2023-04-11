888bc88ff0a152e88e8bfbe9d0ea164ab97686a3
Gennaro Raiola <gennaro.raiola@gmail.com>
2023-04-11 18:31:46 +0200

    Add white list

--- src/ai_utils_ros/detector_factory.py
+++ src/ai_utils_ros/detector_factory.py
@@ -32,6 +32,7 @@ class DetectorFactory:
         score_threshold = rospy.get_param("~score_threshold", 0.6)
         return_image = rospy.get_param("~return_image", True)
         display_image = rospy.get_param("~display_image", False)
+        classes_white_list = rospy.get_param("~classes_white_list", "")
 
         detector = rospy.get_param("~detector", "")
         model_weights = rospy.get_param("~" + detector + "/model_weights", "")
@@ -49,6 +50,8 @@ class DetectorFactory:
         if model_weights == "":
             rospy.logerr("model_weights cannot be an empty string.")
             exit(1)
+        if classes_white_list == "":
+            classes_white_list = set()
 
         self.detector = eval(detector)
 
@@ -58,12 +61,12 @@ class DetectorFactory:
         model_weights = pkg_path + "/weights/" + model_weights
 
         if reid_weights == "":
-            self.detector = self.detector(model_weights=model_weights, return_img=return_image,
-                                          display_img=display_image, score_threshold=score_threshold)
+            self.detector = self.detector(model_weights=model_weights, return_img=return_image, display_img=display_image,
+                                          score_threshold=score_threshold, classes_white_list=classes_white_list)
         else:
             reid_weights = pkg_path + "/weights/" + reid_weights
-            self.detector = self.detector(model_weights=model_weights, reid_weights=reid_weights,
-                                          return_img=return_image, display_img=display_image, score_threshold=score_threshold)
+            self.detector = self.detector(model_weights=model_weights, reid_weights=reid_weights, return_img=return_image,
+                                          display_img=display_image, score_threshold=score_threshold, classes_white_list=classes_white_list)
 
     @property
     def get_detector(self):
